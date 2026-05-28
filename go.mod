10a18b52688e320705627f254ceb7db71d1ff179
edu292 <eduskoroboatei@gmail.com>
2026-05-28 19:19:16 -0300

    Initial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gator
+
+go 1.26.3
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	mux := http.NewServeMux()
+	mux.Handler
+}
